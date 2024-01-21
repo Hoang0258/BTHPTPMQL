e74798111c753d95a83bc9de5a68368bb6c163a8
Hoang0258 <thieuhuyhoang02@gmail.com>
2024-01-21 13:51:20 +0700

    HTML Introduction

--- obj/Debug/net8.0/Huy Hoang.AssemblyInfo.cs
+++ obj/Debug/net8.0/Huy Hoang.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Huy Hoang")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf21f7714ec8cad130c5982f509e9cde6108a58f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d1e56371059f5e1a20fbb018266b39bc2b8d64d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Huy Hoang")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Huy Hoang")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
