c67b040c68613d99f5d4ae993fbf568a69be76ef
Hoang0258 <thieuhuyhoang02@gmail.com>
2024-01-22 21:17:22 +0700

    HTML Form Attributes

--- obj/Debug/net8.0/Huy Hoang.AssemblyInfo.cs
+++ obj/Debug/net8.0/Huy Hoang.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Huy Hoang")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44a8b07e53c3311483f52a162f1f4dbe2ed02f1e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8082125bf94c4240fac4d85fab1ed4bd13ef80b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Huy Hoang")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Huy Hoang")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
